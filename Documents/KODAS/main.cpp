e4d3dd3067e475999b5a8d75d5e99f78220e3f43
adrija <adrija.kalikaite3@gmail.com>
2026-06-17 16:58:21 +0300

    pabaiga

--- Documents/KODAS/main.cpp
+++ Documents/KODAS/main.cpp
@@ -37,9 +37,9 @@ fr.close();
 }
 void rodytiVisasZuvis()
 {
+    cout << "visu zuvu sarasas: " << endl;
     for(int i=0;i<zuvuSkaicius;i++)
     {
-        cout << "Visu zuvu sarasas: " endl;
         cout << "ID: " << zuvys[i].id << endl;
         cout << "Vardas: " << zuvys[i].vardas << endl;
         cout << "Rusis: " << zuvys[i].rusis << endl;
@@ -71,26 +71,56 @@ void pridetiZuvi()
 void redaguotiZuvi()
 {
     int ieskomasId;
-    cout<<"Iveskite zuvies ID: ";
-    cin>>ieskomasId;
-    for(int i=0;i<zuvuSkaicius;i++)
-    { if(zuvys[i].id==ieskomasId)
+    cout << "Iveskite zuvies ID: ";
+    cin >> ieskomasId;
+
+    for (int i = 0; i < zuvuSkaicius; i++)
+    {
+        if (zuvys[i].id == ieskomasId)
         {
-            cout<<"Naujas vardas: ";
-            cin>>zuvys[i].vardas;
-            cout<<"Nauja rusis: ";
-            cin>>zuvys[i].rusis;
-            cout<<"Naujas amzius: ";
-            cin>>zuvys[i].amzius;
-            cout<<"Naujas ilgis: ";
-            cin>>zuvys[i].ilgis;
-            cout<<"Nauja temperatura: ";
-            cin>>zuvys[i].temperatura;
-            cout<<"Naujas kiekis: ";
-            cin>>zuvys[i].kiekis;
-            cout<<"Atnaujinta"<<endl;
-            return;}}
-    cout<<"Zuvis nerasta"<<endl;
+            int pasirinkimas;
+            cout << "Ka norite redaguoti?\n";
+            cout << "1 - Vardas\n";
+            cout << "2 - Rusis\n";
+            cout << "3 - Amzius\n";
+            cout << "4 - Ilgis\n";
+            cout << "5 - Temperatura\n";
+            cout << "6 - Kiekis\n";
+            cin >> pasirinkimas;
+            switch (pasirinkimas)
+            {
+                case 1:
+                    cout << "Naujas vardas: ";
+                    cin >> zuvys[i].vardas;
+                    break;
+                case 2:
+                    cout << "Nauja rusis: ";
+                    cin >> zuvys[i].rusis;
+                    break;
+                case 3:
+                    cout << "Naujas amzius: ";
+                    cin >> zuvys[i].amzius;
+                    break;
+                case 4:
+                    cout << "Naujas ilgis: ";
+                    cin >> zuvys[i].ilgis;
+                    break;
+                case 5:
+                    cout << "Nauja temperatura: ";
+                    cin >> zuvys[i].temperatura;
+                    break;
+                case 6:
+                    cout << "Naujas kiekis: ";
+                    cin >> zuvys[i].kiekis;
+                    break;
+                default:
+                    cout << "Nera tokio pasirinkimo\n";
+            }
+            cout << "Atnaujinta\n";
+            return;
+        }
+    }
+    cout << "Zuvis nerasta\n";
 }
 void pasalintiZuvi()
 {
@@ -137,4 +167,72 @@ void rodytiAtaskaita()
     cout<<endl;
     cout<<"Maziausia:"<<endl;
     cout<<zuvys[maziausia].vardas <<" " <<zuvys[maziausia].kiekis <<endl;
+}
+void spausdintiIFaila()
+{
+    ofstream fr("rezultatas.txt");
+
+    fr << "VISU ZUVU ATASKAITA\n\n";
+
+    for (int i = 0; i < zuvuSkaicius; i++)
+    {
+        fr << "ID: " << zuvys[i].id << "\n";
+        fr << "Vardas: " << zuvys[i].vardas << "\n";
+        fr << "Rusis: " << zuvys[i].rusis << "\n";
+        fr << "Amzius: " << zuvys[i].amzius << "\n";
+        fr << "Ilgis: " << zuvys[i].ilgis << "\n";
+        fr << "Temperatura: " << zuvys[i].temperatura << "\n";
+        fr << "Kiekis: " << zuvys[i].kiekis << "\n";
+        fr << "----------------------\n";
+    }
+
+    fr.close();
+    cout << "Rezultatas issaugotas i rezultatas.txt\n";
+}
+int main()
+{ nuskaitytiIsFailo();
+int pasirinkimas;
+    while(true)
+    {
+        cout<<" Ka norite daryti?" << endl;
+        cout<<"1 - Rodyti visas zuvis"<<endl;
+        cout<<"2 - Prideti zuvi"<<endl;
+        cout<<"3 - Redaguoti zuvi"<<endl;
+        cout<<"4 - Pasalinti zuvi"<<endl;
+        cout<<"5 - Ataskaita"<<endl;
+        cout<<"6 - Issaugoti"<<endl;
+        cout << "7 - Spausdinti ataskaita i faila"<<endl;
+        cout<<"0 - Baigti"<<endl;
+        cin>>pasirinkimas;
+        switch(pasirinkimas)
+        {
+            case 1:
+                rodytiVisasZuvis();
+                break;
+            case 2:
+                pridetiZuvi();
+                break;
+            case 3:
+                redaguotiZuvi();
+                break;
+            case 4:
+                pasalintiZuvi();
+                break;
+            case 5:
+                rodytiAtaskaita();
+                break;
+            case 6:
+                issaugotiIFaila();
+                cout<<"Issaugota"<<endl;
+                break;
+                case 7:
+                  spausdintiIFaila();
+                  break;
+            case 0:
+                issaugotiIFaila();
+                return 0;
+            default:
+                cout<<"NERA TOKIO PASIRINKIMO!";
+        }
+}
 }
