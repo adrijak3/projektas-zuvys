9f1bb27cbf06d50e340d055f1da817382e9bc953
adrija <adrija.kalikaite3@gmail.com>
2026-06-17 15:38:23 +0300

    rodyti/prideti/redaguoti

--- Documents/KODAS/main.cpp
+++ Documents/KODAS/main.cpp
@@ -26,3 +26,69 @@ void nuskaitytiIsFailo()
     }
     fd.close();
 }
+void issaugotiIFaila()
+{ ofstream fr("zuvys.txt");
+  for(int i = 0; i < zuvuSkaicius; i++)
+    { fr << zuvys[i].id << " " << zuvys[i].vardas << " " << zuvys[i].rusis << " "
+        << zuvys[i].amzius << " " << zuvys[i].ilgis << " " << zuvys[i].temperatura << " "
+        << zuvys[i].kiekis << endl;
+    }
+fr.close();
+}
+void rodytiVisasZuvis()
+{
+    for(int i=0;i<zuvuSkaicius;i++)
+    {
+        cout << "Visu zuvu sarasas: " endl;
+        cout << "ID: " << zuvys[i].id << endl;
+        cout << "Vardas: " << zuvys[i].vardas << endl;
+        cout << "Rusis: " << zuvys[i].rusis << endl;
+        cout << "Amzius: " << zuvys[i].amzius << endl;
+        cout << "Ilgis: " << zuvys[i].ilgis << endl;
+        cout << "Temperatura: " << zuvys[i].temperatura << endl;
+        cout << "Kiekis: " << zuvys[i].kiekis << endl;
+    }
+}
+void pridetiZuvi()
+{
+    cout<<"ID: ";
+    cin>>zuvys[zuvuSkaicius].id;
+    cout<<"Vardas: ";
+    cin>>zuvys[zuvuSkaicius].vardas;
+    cout<<"Rusis: ";
+    cin>>zuvys[zuvuSkaicius].rusis;
+    cout<<"Amzius: ";
+    cin>>zuvys[zuvuSkaicius].amzius;
+    cout<<"Ilgis: ";
+    cin>>zuvys[zuvuSkaicius].ilgis;
+    cout<<"Temperatura: ";
+    cin>>zuvys[zuvuSkaicius].temperatura;
+    cout<<"Kiekis: ";
+    cin>>zuvys[zuvuSkaicius].kiekis;
+    zuvuSkaicius++;
+    cout<<"Prideta:)"<<endl;
+}
+void redaguotiZuvi()
+{
+    int ieskomasId;
+    cout<<"Iveskite zuvies ID: ";
+    cin>>ieskomasId;
+    for(int i=0;i<zuvuSkaicius;i++)
+    { if(zuvys[i].id==ieskomasId)
+        {
+            cout<<"Naujas vardas: ";
+            cin>>zuvys[i].vardas;
+            cout<<"Nauja rusis: ";
+            cin>>zuvys[i].rusis;
+            cout<<"Naujas amzius: ";
+            cin>>zuvys[i].amzius;
+            cout<<"Naujas ilgis: ";
+            cin>>zuvys[i].ilgis;
+            cout<<"Nauja temperatura: ";
+            cin>>zuvys[i].temperatura;
+            cout<<"Naujas kiekis: ";
+            cin>>zuvys[i].kiekis;
+            cout<<"Atnaujinta"<<endl;
+            return;}}
+    cout<<"Zuvis nerasta"<<endl;
+}
