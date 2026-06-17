8c0e95fdb2df9024ba2b6befdaf1a85ebbff79e5
adrija <adrija.kalikaite3@gmail.com>
2026-06-17 15:08:57 +0300

    skaitymas

--- Documents/KODAS/main.cpp
+++ Documents/KODAS/main.cpp
@@ -12,4 +12,17 @@ struct Zuvis
     double temperatura;
     int kiekis;
 };
-
+Zuvis zuvys[100];
+int zuvuSkaicius = 0;
+void nuskaitytiIsFailo()
+{
+    ifstream fd("zuvys.txt");
+    while(fd >> zuvys[zuvuSkaicius].id >> zuvys[zuvuSkaicius].vardas >>
+          zuvys[zuvuSkaicius].rusis >> zuvys[zuvuSkaicius].amzius >>
+          zuvys[zuvuSkaicius].ilgis >> zuvys[zuvuSkaicius].temperatura >>
+          zuvys[zuvuSkaicius].kiekis)
+    {
+        zuvuSkaicius++;
+    }
+    fd.close();
+}
