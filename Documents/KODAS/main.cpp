02adce5ee317903dee6792a283284c695e3d9b1a
adrija <adrija.kalikaite3@gmail.com>
2026-06-17 15:03:01 +0300

    pradzia

---
+++ Documents/KODAS/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Zuvis
+{
+    int id;
+    string vardas;
+    string rusis;
+    int amzius;
+    double ilgis;
+    double temperatura;
+    int kiekis;
+};
+
